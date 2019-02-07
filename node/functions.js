ac052f63e3e624d5c4b7c320c16e8be9ad0f6ab9
Nour <nour@axon.im>
2019-02-06 20:28:21 -0800

    function craziness

--- node/functions.js
+++ node/functions.js
@@ -2,6 +2,7 @@
 function hi() {
   console.log('Say Hello');
 }
+// avoid using above way of function declaration, avoid function hoisting, use arrow function below instead.
 
 hi();
 
@@ -15,5 +16,12 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
+const myArrow = (y) => y + 1; // no parantheses mean just return the result of the statement { return y + 1 }
+console.log(myArrow(8));
 
 // make a function that returns a function
+const functionFactory = () => () => console.log('this is weird');
+functionFactory()();
+
+const a = functionFactory();
+a();
